c72df3092afb2f8842b5950d446622cbc17b25d5
Scott Stevenson <scotifer92@googlemail.com>
2018-04-17 11:05:26 +0100

    added IRC functionality with ERC, and RSS with Elfeed.

---
+++ elpa/deferred-0.5.1/deferred-autoloads.el
@@ -0,0 +1,15 @@
+;;; deferred-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("deferred.el") (23247 29759 645344 8000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; deferred-autoloads.el ends here
---
+++ elpa/ipython-shell-send-1.0.2/ipython-shell-send-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "ipython-shell-send" "1.0.2" "Send code (including magics) to ipython shell" '((emacs "24")) :commit "36523a387c15ee1652a5b0e291d4d4838da5e912" :keywords '("tools" "processes") :url "https://github.com/jackkamm/ipython-shell-send-el")
